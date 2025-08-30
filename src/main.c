ee3def43432e86b53153e0294028cf85708dc8fb
sethcg <sethcg@hotmail.com>
2025-08-29 22:43:33 -0400

    added grid

--- src/main.c
+++ src/main.c
@@ -1,70 +1,86 @@
-/*
- * This example code creates an SDL window and renderer, and then clears the
- * window to a different color every frame, so you'll effectively get a window
- * that's smoothly fading between colors.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
+#define SDL_MAIN_USE_CALLBACKS 1 // USE CALLBACKS INSTEAD OF THE "main()" FUNCTION
 
-#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-/* We will use this renderer to draw into this window every frame. */
+// WINDOW CONSTANTS
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
+// GRID CONSTANTS
+const int GRID_SQUARE_SIZE = 32;
+const int GRID_PADDING_SIZE = 16;
+
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-/* This function runs once at startup. */
+void DrawGrid(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);
+
+    // ADD ROWS
+    SDL_FRect row_line = { .x = GRID_PADDING_SIZE, .y = 0, .w = (WINDOW_WIDTH - (GRID_PADDING_SIZE * 2)), .h = 0 };
+    for(row_line.y = GRID_PADDING_SIZE; row_line.y <= (WINDOW_HEIGHT - GRID_PADDING_SIZE); row_line.y += GRID_SQUARE_SIZE) {
+        SDL_RenderRect(renderer, &row_line);
+    }
+
+    // ADD COLUMNS
+    SDL_FRect col_line = { .x = 0, .y = GRID_PADDING_SIZE, .w = 0, .h = (WINDOW_HEIGHT - (GRID_PADDING_SIZE * 2)) };
+    for(col_line.x = GRID_PADDING_SIZE; col_line.x <= (WINDOW_WIDTH - GRID_PADDING_SIZE); col_line.x += GRID_SQUARE_SIZE) {
+        SDL_RenderRect(renderer, &col_line);
+    }
+}
+
+// THIS FUNCTION RUNS ONCE AT STARTUP
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
-
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer))
-    {
+    if (!SDL_CreateWindowAndRenderer("SDL3 Grid", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
-}
-
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_QUIT)
-    {
-        return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
-    }
-    return SDL_APP_CONTINUE; /* carry on with the program! */
-}
-
-/* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
-    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
-
-    /* clear the window to the draw color. */
+    // SET BACKGROUND COLOR (DARK GRAY)
+    SDL_SetRenderDrawColor(renderer, 10, 10, 10, SDL_ALPHA_OPAQUE_FLOAT);
     SDL_RenderClear(renderer);
 
-    /* put the newly-cleared rendering on the screen. */
+    // DRAW GRID
+    DrawGrid(renderer);
+
     SDL_RenderPresent(renderer);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
+// THIS FUNCTION RUNS WHEN A NEW EVENT OCCURS (MOUSE INPUT, KEY PRESS, ETC.)
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event -> type == SDL_EVENT_QUIT)
+    {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+// THIS FUNCTION RUNS ONCE PER FRAME
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    // // SET BACKGROUND COLOR (DARK GRAY)
+    // SDL_SetRenderDrawColorFloat(renderer, 80.0f, 80.0f, 80.0f, SDL_ALPHA_OPAQUE_FLOAT);
+    // SDL_RenderClear(renderer);
+
+    // // DrawGrid(renderer, 16);
+
+    // SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+// THIS FUNCTION RUNS ONCE AT SHUTDOWN
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    /* SDL will clean up the window/renderer for us. */
+    // SDL CLEANS UP WINDOW/RENDERER AUTO-MAGICALLY
 }
