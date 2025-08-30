eeac7294e6938e811af48703de92b7d9ccb28ccc
sethcg <sethcg@hotmail.com>
2025-08-30 10:36:49 -0400

    seperated files, changed build process

--- main.c
+++ main.c
@@ -2,33 +2,13 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <grid.c>
 
-// WINDOW CONSTANTS
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 
-// GRID CONSTANTS
-const int GRID_SQUARE_SIZE = 32;
-const int GRID_PADDING_SIZE = 16;
-
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-
-void DrawGrid(SDL_Renderer *renderer) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);
-
-    // ADD ROWS
-    SDL_FRect row_line = { .x = GRID_PADDING_SIZE, .y = 0, .w = (WINDOW_WIDTH - (GRID_PADDING_SIZE * 2)), .h = 0 };
-    for(row_line.y = GRID_PADDING_SIZE; row_line.y <= (WINDOW_HEIGHT - GRID_PADDING_SIZE); row_line.y += GRID_SQUARE_SIZE) {
-        SDL_RenderRect(renderer, &row_line);
-    }
-
-    // ADD COLUMNS
-    SDL_FRect col_line = { .x = 0, .y = GRID_PADDING_SIZE, .w = 0, .h = (WINDOW_HEIGHT - (GRID_PADDING_SIZE * 2)) };
-    for(col_line.x = GRID_PADDING_SIZE; col_line.x <= (WINDOW_WIDTH - GRID_PADDING_SIZE); col_line.x += GRID_SQUARE_SIZE) {
-        SDL_RenderRect(renderer, &col_line);
-    }
-}
 
 // THIS FUNCTION RUNS ONCE AT STARTUP
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -44,11 +24,11 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
 
     // SET BACKGROUND COLOR (DARK GRAY)
-    SDL_SetRenderDrawColor(renderer, 10, 10, 10, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, SDL_ALPHA_OPAQUE_FLOAT);
     SDL_RenderClear(renderer);
 
     // DRAW GRID
-    DrawGrid(renderer);
+    DrawGrid(renderer, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     SDL_RenderPresent(renderer);
 
@@ -68,14 +48,6 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 // THIS FUNCTION RUNS ONCE PER FRAME
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    // // SET BACKGROUND COLOR (DARK GRAY)
-    // SDL_SetRenderDrawColorFloat(renderer, 80.0f, 80.0f, 80.0f, SDL_ALPHA_OPAQUE_FLOAT);
-    // SDL_RenderClear(renderer);
-
-    // // DrawGrid(renderer, 16);
-
-    // SDL_RenderPresent(renderer);
-
     return SDL_APP_CONTINUE;
 }
 
---
+++ src/grid.c
@@ -0,0 +1,20 @@
+#include <SDL3/SDL.h>
+
+const int GRID_SQUARE_SIZE = 32;
+const int GRID_PADDING_SIZE = 32;
+
+void DrawGrid(SDL_Renderer *renderer, float window_width, float window_height) {
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, SDL_ALPHA_OPAQUE_FLOAT);
+
+    // ADD ROWS
+    SDL_FRect row_line = { .x = GRID_PADDING_SIZE, .y = 0, .w = (window_width - (GRID_PADDING_SIZE * 2)), .h = 0 };
+    for(row_line.y = GRID_PADDING_SIZE; row_line.y <= (window_height - GRID_PADDING_SIZE); row_line.y += GRID_SQUARE_SIZE) {
+        SDL_RenderRect(renderer, &row_line);
+    }
+
+    // ADD COLUMNS
+    SDL_FRect col_line = { .x = 0, .y = GRID_PADDING_SIZE, .w = 0, .h = (window_height - (GRID_PADDING_SIZE * 2)) };
+    for(col_line.x = GRID_PADDING_SIZE; col_line.x <= (window_width - GRID_PADDING_SIZE); col_line.x += GRID_SQUARE_SIZE) {
+        SDL_RenderRect(renderer, &col_line);
+    }
+}
