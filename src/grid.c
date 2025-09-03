e6f4d25c4db31c812e4db5b6499bd56a03fb282e
sethcg <sethcg@hotmail.com>
2025-09-03 06:27:44 -0400

    fixed grid padding

--- src/grid.c
+++ src/grid.c
@@ -4,17 +4,16 @@
 #include <maze.c>
 
 void DrawGrid(SDL_Renderer *renderer, float window_width, float window_height) {
-    const int grid_size = window_width - GRID_PADDING_SIZE;
-    const int square_size = floor(grid_size / GRID_SQUARE_SIZE);
-    const int array_length = pow(square_size, 2);
-
+    const int grid_width = (window_width - GRID_PADDING_SIZE) - GRID_SQUARE_SIZE;
+    const int grid_height = (window_height - GRID_PADDING_SIZE) - GRID_SQUARE_SIZE;
+    const int array_length = grid_width * grid_height;
+    
     int* cells = GenerateMaze(array_length);
 
     // SDL_SetRenderDrawColor(renderer, 200, 200, 200, SDL_ALPHA_OPAQUE_FLOAT);
-
     SDL_FRect square = { .x = 0, .y = 0, .w = GRID_SQUARE_SIZE, .h = GRID_SQUARE_SIZE };
-    for(square.x = GRID_PADDING_SIZE; square.x <= (window_width - GRID_PADDING_SIZE); square.x += GRID_SQUARE_SIZE) {
-        for(square.y = GRID_PADDING_SIZE; square.y <= (window_height - GRID_PADDING_SIZE); square.y += GRID_SQUARE_SIZE) {
+    for(square.x = GRID_PADDING_SIZE; square.x <= grid_width; square.x += GRID_SQUARE_SIZE) {
+        for(square.y = GRID_PADDING_SIZE; square.y <= grid_height; square.y += GRID_SQUARE_SIZE) {
             int value = rand();
             SDL_SetRenderDrawColor(renderer, value, value, value, SDL_ALPHA_OPAQUE_FLOAT);
             SDL_RenderFillRect(renderer, &square);
--- src/headers/grid.h
+++ src/headers/grid.h
@@ -1,4 +1,4 @@
 #pragma once
 
 const int GRID_SQUARE_SIZE = 8;
-const int GRID_PADDING_SIZE = 20;
+const int GRID_PADDING_SIZE = 24;
