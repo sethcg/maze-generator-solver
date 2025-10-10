848348517f2a1d091c25e00cf9a1dd7fedc1e5b8
sethcg <sethcg@hotmail.com>
2025-10-09 21:33:55 -0400

    fixed A* pathfinding/backtracking

--- core/include/AStarPathfinding.h
+++ core/include/AStarPathfinding.h
@@ -5,26 +5,37 @@
 #include <Cell.h>
 
 typedef struct {
+    direction dir;
     int cell_index;
-    int f;  // TOTAL COST OF THE NODE
-    int g;  // DISTANCE BETWEEN THE CURRENT NODE AND THE START NODE
-    int h;  // HEURISTIC - ESTIMATED DISTANCE FROM THE CURRENT NODE TO THE END NODE
+    int f_score;  // TOTAL COST OF THE NODE
+    int g_score;  // DISTANCE BETWEEN THE CURRENT NODE AND THE START NODE
+    int h_score;  // HEURISTIC - ESTIMATED DISTANCE FROM THE CURRENT NODE TO THE END NODE
 } cell_node;
+
+typedef struct {
+    direction dir;
+    int cell_index;
+    int g_score;
+} path_node;
 
 typedef struct PathContext {
     int start;
     int end;
-    int* children;
+    cell_direction* children;
     cell_node current_node;
-    cell_node* openSet;
-    int* closedSet;
+    cell_node* openSet;  
+    path_node* closedSet;
 } PathContext;
 
-#define create_cell_node() { .cell_index = -1, .f = 0, .g = 0, .h = 0 };
+#define create_path_node() { .cell_index = -1, .g_score = 0 };
+
+#define create_cell_node() { .cell_index = -1, .f_score = 0, .g_score = 0, .h_score = 0 };
 
 PathContext* Init_AStarPathfinding(int start, int end, cell* cells);
 
 PathContext* AStarPathfinding(PathContext* pathContext, cell* cells);
+
+PathContext* BacktrackPath(PathContext* pathContext, cell* cells);
 
 static int GetHeuristicValue(int index, int end_index);
 
--- core/src/AStarPathfinding.c
+++ core/src/AStarPathfinding.c
@@ -12,17 +12,18 @@ PathContext* Init_AStarPathfinding(int start, int end, cell* cells) {
     pathContext->start = start;
     pathContext->end = end;
 
-    pathContext->children = (int*) malloc(4 * sizeof(int));
-
+    pathContext->children = (cell_direction*) malloc(4 * sizeof(cell_direction));
     pathContext->openSet = (cell_node*) malloc(GRID_ARRAY_SIZE * sizeof(cell_node));
-    pathContext->closedSet = (int*) malloc(GRID_ARRAY_SIZE * sizeof(int));
+
+    pathContext->closedSet = (path_node*) malloc(GRID_ARRAY_SIZE * sizeof(path_node));
 
     // INITIALIZE EMPTY CELL_NODE ARRAYS
     for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
         cell_node open_node = create_cell_node();
         pathContext->openSet[i] = open_node;
 
-        pathContext->closedSet[i] = -1;
+        path_node closed_node = create_path_node();
+        pathContext->closedSet[i] = closed_node;
     }
 
     return AStarPathfinding(pathContext, cells);
@@ -32,56 +33,73 @@ PathContext* AStarPathfinding(PathContext* pathContext, cell* cells) {
 
     // ADD THE START NODE
     pathContext->openSet[0].cell_index = pathContext->start;
-    pathContext->openSet[0].f = 0;
+    pathContext->openSet[0].g_score = 0;
+    pathContext->openSet[0].h_score = 0;
+    pathContext->openSet[0].f_score = 0;
 
     pathContext->current_node = pathContext->openSet[0];
 
     int open_index = 1;
     int closed_index = 0;
 
-    int test_open[GRID_ARRAY_SIZE];
-    for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
-        test_open[i] = -1;
-    }
-    test_open[0] = pathContext->start;
-
+    int test_close_score[GRID_ARRAY_SIZE];
     int test_closed[GRID_ARRAY_SIZE];
+    direction test_close_dir[GRID_ARRAY_SIZE];
+    for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
+        test_closed[i] = -1;
+        test_close_score[i] = -1;
+        test_close_dir[i] = None;
+    }
 
     while(open_index > 0) {
         // LET THE CURRENT NODE EQUAL THE NODE WITH THE LOWEST 'F' VALUE
         pathContext->current_node = pathContext->openSet[0];
         int remove_index = 0;
         for(int i = 0; i < open_index; i++) {
-            int new_f = pathContext->openSet[i].f;
-            int current_f = pathContext->current_node.f;
+            int new_f = pathContext->openSet[i].f_score;
+            int current_f = pathContext->current_node.f_score;
             if(new_f >= 0 && current_f >= 0 &&  new_f < current_f) {
                 pathContext->current_node = pathContext->openSet[i];
                 remove_index = i;
             }
         }
-        int current_index = pathContext->current_node.cell_index;
 
         // REMOVE CURRENT NODE FROM THE OPEN SET
         for (int i = remove_index; i < open_index; ++i) {
             pathContext->openSet[i] = pathContext->openSet[i + 1];
-            test_open[i] = test_open[i + 1];
         }
         open_index--;
 
         // ADD THE CURRENT NODE TO THE CLOSED SET
-        pathContext->closedSet[closed_index] = current_index;
-        test_closed[closed_index] = current_index;
+        // SET THE DIRECTION TO THE OPPOSITE, FOR BACKTRACKING
+        pathContext->closedSet[closed_index].dir = closed_index == pathContext->start ? None 
+            : pathContext->current_node.dir == Up ? Down
+            : pathContext->current_node.dir == Down ? Up
+            : pathContext->current_node.dir == Right ? Left
+            : pathContext->current_node.dir == Left ? Right
+            : None;
+        pathContext->closedSet[closed_index].cell_index = pathContext->current_node.cell_index;
+        pathContext->closedSet[closed_index].g_score = pathContext->current_node.g_score;
+
+        test_close_dir[closed_index] = closed_index == pathContext->start ? None 
+            : pathContext->current_node.dir == Up ? Down
+            : pathContext->current_node.dir == Down ? Up
+            : pathContext->current_node.dir == Right ? Left
+            : pathContext->current_node.dir == Left ? Right
+            : None;
+        test_close_score[closed_index] = pathContext->current_node.g_score;
+        test_closed[closed_index] = pathContext->current_node.cell_index;
         closed_index++;
 
-        // IF DONE, EXIT
-        if(current_index == pathContext->end) {
+        // IF CURRENT INDEX EQUALS END INDEX, EXIT
+        if(pathContext->current_node.cell_index == pathContext->end) {
             break;
         }
 
         // GET ADJACENT 'CHILD' NODES
         int children_size = GetAdjacentNodes(pathContext, cells);
         for (int i = 0; i < children_size; i++) {
-            int child_index = pathContext->children[i];
+            int child_index = pathContext->children[i].cell_index;
             if(ClosedSetContainsChild(pathContext, child_index)) {
                 continue;
             }
@@ -89,25 +107,66 @@ PathContext* AStarPathfinding(PathContext* pathContext, cell* cells) {
             // CREATE 'F', 'G', 'H' VALUES
             cell_node temp_node = create_cell_node();
             temp_node.cell_index = child_index;
-            temp_node.g = pathContext->current_node.g + 1;
-            temp_node.h = GetHeuristicValue(child_index, pathContext->end);
-            temp_node.f = temp_node.g + temp_node.h;
+            temp_node.dir = pathContext->children[i].dir;
+            temp_node.g_score = pathContext->current_node.g_score + 1;
+            temp_node.h_score = GetHeuristicValue(child_index, pathContext->end);
+            temp_node.f_score = temp_node.g_score + temp_node.h_score;
 
             // CHILD ALREADY IN THE OPEN SET
-            int current_g_value = pathContext->openSet[current_index].g;
-            bool test1 = OpenSetContainsChild(pathContext, child_index);
-            bool test2 = temp_node.g > current_g_value;
             if(OpenSetContainsChild(pathContext, child_index)) {
-                if(temp_node.g > pathContext->openSet[current_index].g) {
+                if(temp_node.g_score > pathContext->current_node.g_score) {
                     continue;
                 }
             }
 
             // ADD CHILD NODE TO OPEN SET
             pathContext->openSet[open_index] = temp_node;
-            test_open[open_index] = temp_node.cell_index;
             open_index++;
         }
+    }
+
+    return BacktrackPath(pathContext, cells);
+}
+
+PathContext* BacktrackPath(PathContext* pathContext, cell* cells) {
+    // GET THE POSITION OF THE END INDEX
+    int current_position = -1;
+    for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
+        if(pathContext->closedSet[i].cell_index == pathContext->end) {
+            current_position = i;
+            break;
+        }
+    }
+
+    int path[GRID_ARRAY_SIZE];
+    for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
+        path[i] = -1;
+    }
+    path[0] = pathContext->end;
+    int running_index = 1;
+
+    // BACKTRACK THROUGH THE INDICES/DIRECTIONS
+    while(current_position > 0) {
+        direction current_dir = pathContext->closedSet[current_position].dir;
+        int current = pathContext->closedSet[current_position].cell_index;
+        int next_index = 
+            current_dir == Up ? current - 1 :
+            current_dir == Down ? current + 1 :
+            current_dir == Left ? current - GRID_ROWS :
+            current_dir == Right ? current + GRID_ROWS :
+            None;
+
+        // RECALCULATE THE CURRENT ARRAY POSITION
+        for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
+            if(pathContext->closedSet[i].cell_index == next_index) {
+                current_position = i;
+                break;
+            }
+        }
+
+        // ADD CELL INDEX TO PATH
+        path[running_index] = next_index;
+        running_index++;
     }
 
     return pathContext;
@@ -123,7 +182,7 @@ static int GetHeuristicValue(int index, int end_index) {
 
 static bool ClosedSetContainsChild(PathContext* pathContext, int child_index) {
     for(int i = 0; i < GRID_ARRAY_SIZE; i++) {
-        if(pathContext->closedSet[i] == child_index) {
+        if(pathContext->closedSet[i].cell_index == child_index) {
             return true;
         }
     }
@@ -146,25 +205,29 @@ static int GetAdjacentNodes(PathContext* pathContext, cell* cells) {
     
     // TOP EDGE
     if(!(cells[cell_index].borders & (1 << Up))) {
-        pathContext->children[array_size] = (cell_index - 1);
+        pathContext->children[array_size].cell_index = (cell_index - 1);
+        pathContext->children[array_size].dir = Up;
         array_size++;
     }
 
     // BOTTOM EDGE
     if(!(cells[cell_index].borders & (1 << Down))) {
-        pathContext->children[array_size] = (cell_index + 1);
+        pathContext->children[array_size].cell_index = (cell_index + 1);
+        pathContext->children[array_size].dir = Down;
         array_size++;
     }
 
     // LEFT EDGE
     if(!(cells[cell_index].borders & (1 << Left))) {
-        pathContext->children[array_size] = (cell_index - GRID_ROWS);
+        pathContext->children[array_size].cell_index = (cell_index - GRID_ROWS);
+        pathContext->children[array_size].dir = Left;
         array_size++;
     }
 
     // RIGHT EDGE
     if(!(cells[cell_index].borders & (1 << Right))) {
-        pathContext->children[array_size] = (cell_index + GRID_ROWS);
+        pathContext->children[array_size].cell_index = (cell_index + GRID_ROWS);
+        pathContext->children[array_size].dir = Right;
         array_size++;
     }
 
